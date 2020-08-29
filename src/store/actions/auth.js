2997c5c77ea7ef399d798a34a9fa0f329a12ba1b
Praveen Kumar <cbkmar92@gmail.com>
2020-08-29 14:27:01 +0530

    moved api key

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -47,9 +47,9 @@ export const auth = (email, password, isSignup) => {
       password: password,
       returnSecureToken: true
     }
-    let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA_IejgaLCzWbd9MSPLZdCS5y12SEc80Yc";
+    let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + process.env.REACT_APP_GOOGLE_API;
     if (!isSignup) {
-      url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA_IejgaLCzWbd9MSPLZdCS5y12SEc80Yc"
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + process.env.REACT_APP_GOOGLE_API
     }
 
     axios.post(url, authData)
@@ -89,7 +89,6 @@ export const authCheckState = () => {
       } else {
         dispatch(logout())
       }
-
     }
   }
 }
